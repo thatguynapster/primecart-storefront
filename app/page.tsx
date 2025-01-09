4c1a4e8fb92d45d3a4ba6abca703c02fe1c2a0db
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-01-09 17:07:18 +0000

    added discount banner component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ProductCategories from "@/components/product-categories";
 import { Slider } from '@/components/products-slider';
 import ProductCard from '@/components/product-card';
-import Image from 'next/image';
 
 type Props = {}
 
@@ -21,7 +20,7 @@ const Home = async ({ }: Props) => {
           options={{ showButtons: true, rows: 2 }}
         >
           {Array.from({ length: 10 }, (_, i) =>
-            <ProductCard key={i} />
+            <ProductCard product={{ id: 'some_product_id' }} key={i} />
           )}
         </Slider>
       </div>
@@ -34,26 +33,12 @@ const Home = async ({ }: Props) => {
           options={{ showButtons: true, rows: 2 }}
         >
           {Array.from({ length: 10 }, (_, i) =>
-            <ProductCard key={i} />
+            <ProductCard product={{ id: 'some_product_id' }} key={i} />
           )}
         </Slider>
       </div>
 
-      {/* <div className="flex flex-col gap-2 5 py-8 px-6 rounded-xl relative w-full h-44">
-        <Image
-          src={'https://primecart.s3.us-east-2.amazonaws.com/categories/Rectangle 109.png-1734110797092.png'}
-          alt={"Business Name Logo"}
-          priority
-          fill
-          sizes="(max-width: 1200px) 100vw, (max-width: 768px) 50vw, 33vw"
-          className="z-[-1] object-cover mx-auto rounded-xl"
-        />
-
-        <h1 className="text-2xl font-medium text-white font-rubik">
-          7% Discount
-        </h1>
-        <p className="text-lg text-white leading-6">Get 7% off your first purchase when you sign up for our exclusive promotions, updates and news</p>
-      </div> */}
+      {/* <DiscountBanner /> */}
     </main>
   )
 }
