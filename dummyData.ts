a4680e33b0d1a4807fbd14b0f0aef22ff0d41327
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-01-09 17:18:50 +0000

    updated products categories component

---
+++ dummyData.ts
@@ -0,0 +1,22 @@
+export const categories: { previewImage: string; name: string }[] = [
+  {
+    name: "category 1",
+    previewImage:
+      "https://primecart.s3.us-east-2.amazonaws.com/categories/Rectangle 109.png-1734110797092.png",
+  },
+  {
+    name: "category 2",
+    previewImage:
+      "https://primecart.s3.us-east-2.amazonaws.com/categories/Rectangle 109.png-1734110797092.png",
+  },
+  {
+    name: "category 3",
+    previewImage:
+      "https://primecart-dev.thatguynapster.com/_next/image?url=https%3A%2F%2Fprimecart.s3.us-east-2.amazonaws.com%2Fproducts%2FScreenshot%20from%202024-11-06%2015-00-02.png-1733505829202.png&w=640&q=75",
+  },
+  {
+    name: "category 4",
+    previewImage:
+      "https://primecart.s3.us-east-2.amazonaws.com/categories/Rectangle 109.png-1734110797092.png",
+  },
+];
