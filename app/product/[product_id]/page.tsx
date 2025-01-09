ec467ff70d8bf7177323eef86f1575f970b2ea7e
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-01-09 17:32:56 +0000

    added product details page

---
+++ app/product/[product_id]/page.tsx
@@ -0,0 +1,17 @@
+import { ImagePreviewSlider } from '@/components/image-preview-slider'
+import { productImages } from '@/dummyData'
+import React from 'react'
+
+type Props = {
+    params: { product_id: string }
+}
+
+const page = ({ params: { product_id } }: Props) => {
+    return (
+        <div className="px-4">
+
+        </div>
+    )
+}
+
+export default page
