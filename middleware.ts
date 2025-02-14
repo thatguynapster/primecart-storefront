41a6701e0dc8049939c17e9fa486eaf6bc9b338f
Andrew Osei <andrewosei94@gmail.com>
2025-02-14 15:10:44 +0000

    modified middleware to get business details from domain or subdomain

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,23 @@ import { getBusiness } from "./lib/queries";
 
 export default async function middleware(req: NextRequest, res: NextResponse) {
   const hostname = req.headers.get("host") ?? "";
-  const subdomain = hostname.split(".")[0];
+
+  const subdomain =
+    hostname.split(".").length > 1 ? hostname.split(".")[0] : null;
 
   // TODO: add this back when UIs are ready
-  // // get business id and return in headers
-  // const business = await getBusiness(subdomain);
-  // console.log(business);
+  // get business id and return in headers
+  let business;
+
+  if (subdomain) {
+    business = await getBusiness({ subdomain });
+  } else {
+    business = await getBusiness({ domain: hostname });
+  }
 
   // Response
   const response = NextResponse.next();
-  response.headers.append("business", "business.data");
+  response.headers.append("business", business.data);
 
   return response;
 }
